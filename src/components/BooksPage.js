af93150a299311d053d7af1b670cc9d82fa3b871
Sakshi Kale <163717228+sakshikaleee@users.noreply.github.com>
2024-05-17 18:27:49 +0530

    Update BooksPage.js

--- src/components/BooksPage.js
+++ src/components/BooksPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './BooksPage.css'; // Import CSS file
+import './BooksPage.css'; 
 
 function BooksPage() {
   const [books, setBooks] = useState([]);
